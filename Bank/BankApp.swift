1a3d61366e4c6492d256104023d81a070deb7bbe
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-06-29 13:45:22 +0300

    Initial Commit

---
+++ Bank/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  Bank
+//
+//  Created by Сергей Дятлов on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
